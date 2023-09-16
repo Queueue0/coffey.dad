60c6a5d26df5f93f9b46d0edea37d9cbf20b4898
Queueue0 <jcoffey97@gmail.com>
2023-09-16 14:50:08 -0400

    Added preview when adding new blog post

---
+++ static/js/md_preview.js
@@ -0,0 +1,7 @@
+update_preview = () => {
+  bodyBox = document.getElementById("bodybox");
+  previewBox = document.getElementById("preview");
+  converter = new showdown.Converter();
+
+  previewBox.innerHTML = converter.makeHtml(bodyBox.value);
+}
